d63ae97b3320d9aaca8b60f299a88b264dc85dd2
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-04-25 03:32:21 +0300

    Update run.js

--- __node_js__/__clean_code__/run.js
+++ __node_js__/__clean_code__/run.js
@@ -13,20 +13,53 @@ const path = process.argv[2];
 
 // Get file content
 async function getContent(path){
-    return fs.readFileSync(path, 'utf-8');
+    return await fs.readFileSync(path, 'utf-8');
 }
 
 // Overwrite file content
-function writeContent(path, content){
-    fs.writeFileSync(path, content);
+async function writeContent(path, content){
+    await fs.writeFileSync(path, content);
 }
 
-// Cleanup comments
+// Clean comments
 function cleanComments(content){
     return content.replaceAll(/(\/\/.*)|(\/\*[\s\S]*?\*\/)/g, "");
 }
 
+// Cleanup whitespace
+function cleanWhitespace(text) {
+    let inQuotes = false;
+    let currentQuote = null;
+    let cleanedText = '';
+  
+    for (const char of text) {
+        if ((char === '"' || char === "'" || char === '`') && (currentQuote == null || currentQuote === char)) {
+            inQuotes = !inQuotes;
+            if(inQuotes){
+                currentQuote = char;
+            }else{
+                currentQuote = null;
+            }
+            cleanedText += char;
+        } else if (char.match(/\s/) && !inQuotes) {
+            if (cleanedText.length === 0 || cleanedText.slice(-1) !== ' ') {
+                cleanedText += ' '; // Add only a single space as needed
+            }
+        } else {
+            cleanedText += char;
+        }
+    }
+  
+    return cleanedText;
+}
+
+// Cleanup comments
+async function cleanupContent(path){
+    let content = await getContent(path);
+    content = cleanComments(content);
+    content = cleanWhitespace(content);
+    await writeContent(path, content);
+}
+
 // Cleanup code!
-let content = getContent(path);
-let newContent = cleanComments(content);
-writeContent(path, newContent);
+cleanupContent(path);

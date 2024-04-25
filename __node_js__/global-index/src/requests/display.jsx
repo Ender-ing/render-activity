a0645bee29614cf440657140bdd249f07176ea86
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-04-25 07:05:54 +0300

    Update error pages

--- __node_js__/global-index/src/requests/display.jsx
+++ __node_js__/global-index/src/requests/display.jsx
@@ -8,13 +8,17 @@ import { DIALOG, showDialog } from "../content/dialogs";
 import { localiseContent } from "./language/process";
 
 //  Get display content!
+const PAGES = {
+    ERROR_404: "https://resources.ender.ing/web/client/global-pages/404-error/",
+    ERROR_CODE: "https://resources.ender.ing/web/client/global-pages/code-error/"
+}
 export async function getDisplay(base){
     let displayURL = getURL(base);
     let XML;
     try {
         XML = await fetchDisplay(displayURL, fixBase(base));
     }catch(e) {
-        XML = await fetchDisplay("https://resources.ender.ing/web/client/global-pages/error.display", fixBase(base));
+        XML = await fetchDisplay(PAGE.ERROR_CODE, PAGE.ERROR_CODE);
     }
     return XML;
 }
@@ -38,7 +42,7 @@ function getURL(base){
 }
 
 // Get the XML content from .display file
-function fetchDisplay(url, pathname){
+function fetchDisplay(url, pathname, text = false){
     console.log(`Fetching ${url}`);
     return new Promise((resolve, reject) => {
         fetch(url, {
@@ -47,22 +51,33 @@ function fetchDisplay(url, pathname){
                 // This header must be present to prevent a global HTML page from being served!
                 'x-display-request': 1
             }
-        }).then(response => {
-            if (!response.ok) {
+        }).then(async response => {
+            if(response.status === 404 && pathname !== PAGES.ERROR_404){
+                pathname = PAGES.ERROR_404;
+                return await fetchDisplay(PAGES.ERROR_404, PAGES.ERROR_404, true);
+            }else if (!response.ok) {
                 showDialog(DIALOG.network.error);
                 throw new Error('Network response was not ok');
             }
             return response.text();
         }).then(async xmlString => {
-            let localisedXML = await localiseContent(xmlString, pathname);
-            return localisedXML;
+            if(!text){
+                let localisedXML = await localiseContent(xmlString, pathname);
+                return localisedXML;
+            }else{
+                return xmlString;
+            }
         }).then(xmlString => {
-            // Parse the XML string
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+            if(!text){
+                // Parse the XML string
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(xmlString, "text/xml");
     
-            // Extract the values you need
-            return xmlDoc;
+                // Extract the values you need
+                return xmlDoc;
+            }else{
+                return xmlString;
+            }
         }).then(xml => {
             // Prevent callback function errors from triggering the catch function!
             try {

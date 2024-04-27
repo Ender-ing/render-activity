0e138414167d09f72369ef2187d0947bcc8d01eb
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-04-27 18:06:04 +0300

    Add Google Tag

---
+++ __node_js__/global-index/public/tracking.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Manage Google Analytics tracking
+ * 
+**/
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-5F32MWD0EQ');
+gtag('set', 'allowAdFeatures', false);
+gtag('send', 'pageview');

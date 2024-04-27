fd2f1ce763e262556389bd6d2b289be701936c62
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-04-27 18:30:05 +0300

    Disable 3rd party cookies

--- __node_js__/global-index/public/tracking.js
+++ __node_js__/global-index/public/tracking.js
@@ -9,7 +9,11 @@ function gtag(){
     dataLayer.push(arguments);
 }
 
+// Default code
 gtag('js', new Date());
 gtag('config', 'G-5F32MWD0EQ');
-gtag('set', 'allowAdFeatures', false);
 gtag('send', 'pageview');
+// Disable cookies
+gtag('config', 'G-5F32MWD0EQ', { 'storage': 'none' });
+// Disable Google Ads Tracking
+gtag('set', 'allowAdFeatures', false);

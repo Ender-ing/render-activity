85661a6479b72b33abeef22af2a7d5c4da64bba4
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-04-29 05:52:05 +0300

    Update INSTALL-DEP.bat

--- INSTALL-DEP.bat
+++ INSTALL-DEP.bat
@@ -1,9 +1,22 @@
 @echo off
 
+:: Get environment variables
+FOR /F "tokens=*" %%i in (BUILD.env) do SET %%i
+
 cls
-echo [101;93m INSTALLING GLOBAL NODEJS DEPENDENCIES! [0m
+echo [101;93m INSTALLING NODEJS DEPENDENCIES! [0m
+
+echo [44;45m INSTALLING GLOBAL NODEJS DEPENDENCIES... [0m
+
+pushd %BUILD_PATH%\__node_js__\material-imports
+CMD "Running Backup" /C "npm i"
+popd
+pushd %BUILD_PATH%\__node_js__\global-index
+CMD "Running Backup" /C "npm i"
+popd
+
+echo [44;45m INSTALLING LOCAL NODEJS DEPENDENCIES... [0m
 
 CMD "Running Backup" /C "npm install -g terser@latest"
-CMD "Running Backup" /C "npm install clean-css-cli -g"
 
-echo [101;93m GLOBAL NODEJS DEPENDENCIES INSTALLED! [0m
+echo [101;93m NODEJS DEPENDENCIES INSTALLED! [0m

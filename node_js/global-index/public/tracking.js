f95545e35afb0eeef573ff540815261e68c55461
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-05-03 22:19:29 +0300

    Fix a bug
    Google Tag Manage still keeps ignoring cookies storage settings

--- node_js/global-index/public/tracking.js
+++ node_js/global-index/public/tracking.js
@@ -4,16 +4,45 @@
  * 
 **/
 
+// Define dataLayer object
+const GTag = "G-5F32MWD0EQ";
 window.dataLayer = window.dataLayer || [];
 function gtag(){
     dataLayer.push(arguments);
 }
 
-// Default code
+// Define script injection code
+function injectGTag(){
+    let script = document.createElement('script');
+    script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=" + GTag);
+    script.setAttribute("async", "");
+    // Append script
+    document.body.appendChild(script);
+}
+
+// Await ready state
+function awaitReadyState(callback) {
+    // Check if the document is already ready
+    if(document.readyState === 'ready' || document.readyState === 'complete'){
+        callback();
+    }else{
+        // Await ready state change
+        document.onreadystatechange = function(){
+            if(document.readyState == "complete"){
+                callback();
+            }
+        }
+    }
+}
+
+// Default setup
 gtag('js', new Date());
-gtag('config', 'G-5F32MWD0EQ');
+gtag('config', GTag);
 gtag('send', 'pageview');
 // Disable cookies
-gtag('config', 'G-5F32MWD0EQ', { 'storage': 'none' });
+gtag('config', GTag, { 'storage': 'none' });
 // Disable Google Ads Tracking
 gtag('set', 'allowAdFeatures', false);
+
+// Inject Tag
+awaitReadyState(injectGTag);

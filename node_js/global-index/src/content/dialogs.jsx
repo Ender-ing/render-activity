bcf382df2f3814358edc3ca91faa7706b125504f
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-05-07 04:49:17 +0300

    Hide dialogs by default

--- node_js/global-index/src/content/dialogs.jsx
+++ node_js/global-index/src/content/dialogs.jsx
@@ -24,8 +24,24 @@ function showDialogElm(elm){
     elm.open = true;
 }
 
+// Show dialog activity
+let dialogActivity = null;
+async function awaitDialogActivity(){
+    if(dialogActivity == null){
+        dialogActivity = document.getElementById("dialogs-activity");
+    }
+    if (dialogActivity != null) {
+        // Show dialogs activity
+        dialogActivity.style.display = null;
+        return null;
+    }
+    await new Promise((resolve) => setTimeout(resolve, window.CHECK_DOM_LOOP_DELAY)); // Check again on next cycle
+    return awaitDialogActivity();
+}
+
 // Show a dialog specific to an error
 export async function showDialog(id){
     await awaitVisibility();
+    await awaitDialogActivity();
     showDialogElm(document.getElementById(id));
 }

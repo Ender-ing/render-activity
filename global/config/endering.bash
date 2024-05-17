49c120c112cd1a9634b5f3c3c0d05df1f66d726e
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-05-17 08:02:47 +0300

    Update endering.bash

--- global/config/endering.bash
+++ global/config/endering.bash
@@ -46,7 +46,7 @@ if [ "$1" == "help" ]; then
     printf "\t%-18s %s\n" "*clean-records" "Clean up record files (Only use when sure!)"
     printf "\t%-18s %s\n" "fix-perms" "Fix files permissions"
     echo -e "\n Quick commands:"
-    printf "\t%-18s %s\n" "web" "(get & cache)"
+    printf "\t%-18s %s\n" "web" "(get & fix-perms & cache)"
     echo -e "\n"
     echo "(Do not spam these commands!)"
 elif [ "$1" == "get" ]; then
@@ -143,6 +143,7 @@ elif [ "$1" == "fix-perms" ]; then
 elif [ "$1" == "web" ]; then
     # Update files and purge cache
     $cmd get
+    $cmd fix-perms
     $cmd cache
 else
     echo "Invalid command! Use the command '(endering) help' to see valid commands."

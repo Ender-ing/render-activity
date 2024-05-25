1b1755a88f482d6365ffabd00e90eb50884c29ef
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-05-26 02:57:03 +0300

    Add soft errors

--- node_js/utility/_console.js
+++ node_js/utility/_console.js
@@ -6,6 +6,8 @@
 
 const colors = require('colors');
 
+const newLine = "\n\t";
+
 // Log normal messages!
 function log(...args){
     console.log(...args);
@@ -13,27 +15,36 @@ function log(...args){
 
 // Show informative text
 function info(...args){
-    console.log(args.join("\n").blue);
+    console.log(args.join(newLine).blue);
 }
 
 // Show action text
 function action(...args){
-    console.log(args.join("\n").green);
+    console.log(args.join(newLine).green);
 }
 
 // Show warnings
 function warn(...args){
-    console.warn(args.join("\n").yellow);
+    console.warn(args.join(newLine).yellow);
 }
 
 // Show errors and end script!
-function error(msg){
-    if(typeof msg == 'string'){
-        console.error(msg.red);
-        throw new Error(msg);
+const CONSOLE_SOFT_ERROR = "~~CONSOLE=>ERROR-SOFT~~";
+function error(...args){
+    // Check flag
+    let flagIndex = args.indexOf(CONSOLE_SOFT_ERROR);
+    if(flagIndex != -1){
+        args.slice(flagIndex, flagIndex);
+    }
+    if(typeof args[0] == 'string'){
+        console.error(args.join(newLine).red);
+        // throw new Error(msg);
     }else{
-        console.error(msg.stack.red);
-        throw new Error(msg);
+        console.error(args[0].stack.red);
+        throw new Error(args[0]);
+    }
+    if(flagIndex == -1){
+        process.exit(1);
     }
 }
 
@@ -42,5 +53,6 @@ module.exports = {
     info,
     action,
     warn,
+    CONSOLE_SOFT_ERROR,
     error
 };

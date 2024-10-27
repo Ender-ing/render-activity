18cfcdd362625f0ea6a8b0af196582caca9d4e62
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-27 16:20:19 +0200

    Update docs.js

--- node_js/material-imports/docs.js
+++ node_js/material-imports/docs.js
@@ -4,4 +4,12 @@
  * 
 **/
 
+// Buttons
 import '@material/web/button/elevated-button.js';
+
+// Chips
+import '@material/web/chips/chip-set.js';
+import '@material/web/chips/assist-chip.js';
+
+// Focus (effect)
+import '@material/web/focus/md-focus-ring.js';

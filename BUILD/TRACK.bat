aebd3401ac519043ef62415a72591037dbfec8a2
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-04 14:44:03 +0200

    Update build.txt file

--- BUILD/TRACK.bat
+++ BUILD/TRACK.bat
@@ -18,12 +18,34 @@ if %errorlevel% NEQ 0 ( set errorTrigger="level" && goto local_bat_error )
 :: Change to the target directory 
 pushd %OUTPUT_PATH%
 
+:: Create the file and add a note to it!
+echo ^(?) This is an internal file, do NOT share its contents. > build.txt
+
 :: Generate a random 32-character hexadecimal string using PowerShell
-powershell -Command "[System.Guid]::NewGuid().ToString('N')" > build.txt
+for /f "tokens=*" %%a in ('powershell -Command "[System.Guid]::NewGuid().ToString('N')"' ) do set "randomHash=%%a"
+
+:: Get the local timezone and time
+for /f "tokens=*" %%a in ('powershell "(Get-TimeZone).DisplayName"') do set timezone=%%a
+for /f "tokens=*" %%a in ('powershell Get-Date') do set fulldate=%%a
 
 :: Echo the time to the build file
 :: (Need to actually add this later on...)
-echo "YYYY-MM-DD HH:MM:SS TT" >> build.txt
+echo ^(!) Build info: >> build.txt
+echo Build R-Hash:              %randomHash% >> build.txt
+echo Build Timezone:            %timezone% >> build.txt
+echo Build Time:                %fulldate% >> build.txt
+
+:: Get all system info
+set sysInfo=(systeminfo)
+
+:: Get info about the person who built this!
+echo ^(!) User info: >> build.txt
+echo User Name:                 %USERNAME% >> build.txt
+%sysInfo% | findstr /i /c:"Host Name" >> build.txt
+
+:: Get basic system info
+echo ^(!) System info: >> build.txt
+%sysInfo% | findstr /i /c:"OS Name" /c:"OS Version" /c:"System Manufacturer" /c:"System Model" /c:"System Type" /c:"Processor(s)" /c:"BIOS Version / Date" /c:"Total Physical Memory"  >> build.txt
 
 :: Return to the original directory
 popd

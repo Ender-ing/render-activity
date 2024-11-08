307ec2f0dd566dcb6273b5ce14c9bbd1eccf9a86
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-08 23:21:29 +0200

    Update _files.js

--- node_js/utility/_files.js
+++ node_js/utility/_files.js
@@ -121,7 +121,6 @@ async function folderExists(path){
 
 // Rename the name of a folder
 async function renameFolder(oldPath, newPath){
-    console.log("PATHS:", oldPath, newPath);
     try{
         await fs.promises.rename(oldPath, newPath);
         return true;
